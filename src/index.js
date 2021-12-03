f08b6e1699602c56dcd7a747286b391efc46acd4
kyle <keginzburg@gmail.com>
2021-12-02 19:16:05 -0500

    Continue orbit canvas and moon class

--- src/index.js
+++ src/index.js
@@ -1,24 +1,64 @@
 const Planet = require("./planet.js");
+const Moon = require("./moon.js");
 
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', () => {
   const orbitCanvas = document.querySelector(".orbit_canvas");
   orbitCanvas.width = 500;
   orbitCanvas.height = 500;
+
   const ctx = orbitCanvas.getContext('2d');
+  const ctx2 = orbitCanvas.getContext('2d');
+
 
   function space() {
     const spaceColor = "rgba(20,29,33,255)";
     ctx.fillStyle = spaceColor;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, orbitCanvas.width, orbitCanvas.height);
   }
 
   let planet;
+  let moons;
+
+  function captureMoons(moonData) {
+    moons = [];
+
+    if (moonData != null)
+      for (let i = 0; i < moonData.length; i++) {
+        let moonURL = moonData[i].rel;
+        fetch(moonURL)
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            if (data.meanRadius > data.equaRadius) {
+              let moonRadius = data.meanRadius;
+              moons.push(new Moon(orbitCanvas.width / 2, orbitCanvas.height / 2, Math.ceil(moonRadius * 0.001), Math.random() * Math.PI * 2, Math.random() * 100 + 100, Math.random() * 100 + 100, (27 / data.sideralOrbit) * 0.003));
+            } else {
+              let moonRadius = data.equaRadius;
+              moons.push(new Moon(orbitCanvas.width / 2, orbitCanvas.height / 2, Math.ceil(moonRadius * 0.001), Math.random() * Math.PI * 2, Math.random() * 100 + 100, Math.random() * 100 + 100, (27 / data.sideralOrbit) * 0.009));
+            }
+          })
+
+        //let eccentricity = getMoonData(moonURL);
+        // eccentricity = c/a
+        // c is distance from center of ellipse to focus
+        // a is semimajoraxis
+        // find out the x axis distance shift and add it the x coordinate
+        // possibly change the velocity depending on its eccentricity? or just give it a blanket speed according to its sideral orbital data point?
+
+        // moons.push(new Moon(orbitCanvas.width / 2, orbitCanvas.height / 2, Math.random() * 2, "rgba(200,65,47,255)", Math.random() * Math.PI * 2, Math.random() * 100 + 100, Math.random() * 100 + 100, Math.random()*0.003));
+      }
+  }
 
   function animate() {
     requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, orbitCanvas.width, orbitCanvas.height);
     space();
+
     planet.updatePlanet(ctx);
+    moons.forEach(moon => {
+      moon.updateMoon(ctx2);
+    })
   }
 
   function getPlanetData(planetName) {
@@ -27,7 +67,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
         return response.json()
       })
       .then(data => {
-        planet = new Planet(canvas.width / 2, canvas.height / 2, data.meanRadius * 0.001);
+        planet = new Planet(orbitCanvas.width / 2, orbitCanvas.height / 2, data.meanRadius * 0.001);
+        captureMoons(data.moons);
         animate();
       });
   }
---
+++ src/moon.js
@@ -0,0 +1,31 @@
+function Moon(x, y, radius, color, radians, distanceX, distanceY, velocity) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.color = "rgba(19,226,79,255)";
+  this.radians = radians;
+  this.distanceX = distanceX;
+  this.distanceY = distanceY;
+  this.velocity = velocity;
+
+  this.updateMoon= (ctx2) => {
+    this.radians += this.velocity;
+    this.x = x + Math.cos(this.radians) * this.distanceX;
+    this.y = y + Math.sin(this.radians) * this.distanceY;
+    this.drawMoon(ctx2);
+  }
+
+  this.drawMoon = (ctx2) => {
+    ctx2.beginPath();
+    ctx2.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx2.strokeStyle = this.color;
+    ctx2.fillStyle = this.color;
+    ctx2.shadowBlur = 5;
+    ctx2.shadowColor = this.color;
+    ctx2.stroke();
+    ctx2.fill();
+    ctx2.closePath();
+  }
+}
+
+module.exports = Moon;
