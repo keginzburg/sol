ba2b0d3711a133624f14e78b7c3e199ef0d20f61
kyle <keginzburg@gmail.com>
2021-12-06 14:50:03 -0500

    Add basic bar chart to readout data section

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 const simulateOrbit = require("./scripts/orbitSim.js");
 const simulateGravity = require("./scripts/gravitySim.js");
 const simulateTemp = require("./scripts/tempSim.js");
+const renderChart = require("./scripts/dataChart.js");
 
 document.addEventListener('DOMContentLoaded', () => {
   let navBar = document.querySelector(".nav_bar");
+
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Moons', 'Grav', 'Temp'],
+      datasets: [{
+        label: "",
+        data: [],
+        backgroundColor: [
+          "rgba(rgba(19,226,79,255)"
+        ],
+        borderColor: [
+          "rgba(19, 226, 79, 255)"
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      plugins: {
+        legend: {
+          display: false,
+        }
+      }
+    }
+  });
+
   navBar.addEventListener('click', function(event) {
     let planetName = event.target.innerText;
 
@@ -16,6 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
             simulateOrbit(data);
             simulateGravity(data);
             simulateTemp(data);
+            renderChart(myChart, data);
         });
     }
     getPlanetData(planetName);
---
+++ src/scripts/dataChart.js
@@ -0,0 +1,36 @@
+
+function renderChart(chart, fetchedData) {
+  let myChart = chart;
+  let moons;
+  if (fetchedData.moons != null) {
+    moons = fetchedData.moons.length;
+  } else {
+    moons = 0;
+  }
+  let gravity = fetchedData.gravity;
+  let temperature = fetchedData.avgTemp;
+
+  function addData(chart, planetData) {
+    chart.data.datasets.forEach((dataset) => {
+      dataset.data.push(planetData);
+    })
+    chart.update();
+  }
+
+  function removeData(chart) {
+    chart.data.datasets.forEach((dataset) => {
+      dataset.data.pop();
+    });
+    chart.update();
+  }
+  removeData(myChart);
+  removeData(myChart);
+  removeData(myChart);
+  addData(myChart, moons);
+  addData(myChart, gravity);
+  addData(myChart, temperature);
+
+}
+
+module.exports = renderChart;
+
