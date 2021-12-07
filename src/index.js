03c34bf9acfae62fa3eac4e1f8135aa62aa305b8
kyle <keginzburg@gmail.com>
2021-12-07 14:15:17 -0500

    Complete basic intro modal with working buttons for close and open

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ const renderChart = require("./scripts/dataChart.js");
 const renderData = require("./scripts/renderData.js");
 
 document.addEventListener('DOMContentLoaded', () => {
-  let navBar = document.querySelector(".nav_bar");
-
   const ctx = document.getElementById('myChart').getContext('2d');
 
   const myChart = new Chart(ctx, {
@@ -59,6 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
   ctx.shadowBlur = 4;
   ctx.shadowColor = "rgba(19, 226, 79, 255)";
 
+  const navBar = document.querySelector(".nav_bar_planet_buttons");
   navBar.addEventListener('click', function(event) {
 
     let planetName = event.target.innerText;
@@ -80,17 +79,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
   })
 
+  const modalCloseButton = document.querySelector(".close_modal_button");
+  modalCloseButton.addEventListener('click', function () {
+    const modal = document.querySelector(".modal_container");
+    modal.setAttribute("hidden", true);
+  })
 
-  // function getPlanetData(planetName='earth') {
-  //   fetch(`https://api.le-systeme-solaire.net/rest/bodies/${planetName}`)
-  //   .then(response => {
-  //     return response.json();
-  //   })
-  //   .then(data => {
-  //     simulateOrbit(data);
-  //     simulateGravity(data);
-  //     simulateTemp(data);
-  //   });
-  // }
-  // getPlanetData("jupiter");
+  const modalOpenButton = document.querySelector(".open_modal_button");
+  modalOpenButton.addEventListener('click', function () {
+    const modal = document.querySelector(".modal_container");
+    modal.removeAttribute("hidden");
+  })
+
 })
